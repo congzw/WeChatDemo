9403d1ff315ac34db29fab91e03c5c8a02d7286a
congzw <2981810300@qq.com>
2019-02-21 10:34:28 +0800

    add datetime extensions

---
+++ src/CommonFx/Common/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CommonFx.Common
+{
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// 格式化时间
+        /// </summary>
+        /// <param name="datetime"></param>
+        /// <param name="format"></param>
+        /// <returns></returns>
+        public static string ToFormat(this DateTime datetime, string format = "yyyy-MM-dd HH:mm:ss")
+        {
+            return datetime.ToString(format);
+        }
+
+        /// <summary>
+        /// 格式化日期
+        /// </summary>
+        /// <param name="datetime"></param>
+        /// <param name="format"></param>
+        /// <returns></returns>
+        public static string ToDateFormat(this DateTime datetime, string format = "yyyy-MM-dd")
+        {
+            return datetime.ToString(format);
+        }
+    }
+}
