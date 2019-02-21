9775b50cba5f42e20189845b5722743a2d3e35d0
congzw <2981810300@qq.com>
2019-02-21 10:50:26 +0800

    add get GetFocusLoginUser place holder

--- src/MvcApp.Web/Areas/Auths/Controllers/AccountController.cs
+++ src/MvcApp.Web/Areas/Auths/Controllers/AccountController.cs
@@ -1,10 +1,14 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
+using CommonFx.Common;
+using CommonFx.Common.Ioc;
 using CommonFx.Common.Security;
+using CommonFx.Common.Web.Mvc;
 using MvcApp.Web.Areas.Auths.Models;
 
 namespace MvcApp.Web.Areas.Auths.Controllers
 {
-    public class AccountController : Controller
+    public class AccountController : MyControllerBase
     {
         private readonly IAuthManager _authManager;
 
@@ -61,10 +65,36 @@ namespace MvcApp.Web.Areas.Auths.Controllers
             _authManager.SignOut();
             return RedirectToAction("Index", "Home");
         }
-        
+
         public ActionResult _FocusLogin()
         {
             return PartialView();
+        }
+
+        public ActionResult GetFocusLoginUser(string senceId)
+        {
+            var loginSuccess = CheckFocusLogin(senceId);
+            if (loginSuccess)
+            {
+                var authManager = CoreServiceProvider.LocateService<IAuthManager>();
+                authManager.SignOut();
+                authManager.SignIn("MockUser" + DateTime.Now.ToFormat(), false);
+                return MyJson("MockUser");
+            }
+            return MyJson<string>(null);
+        }
+
+        private static int invokeCount = 0;
+        private static bool CheckFocusLogin(string senceId)
+        {
+            //todo wechat api logic!
+            invokeCount++;
+            if (invokeCount >= 5)
+            {
+                invokeCount = 0;
+                return true;
+            }
+            return false;
         }
     }
 }
